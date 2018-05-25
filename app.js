bd4ccdafc8e3aabbe199e83a94de495d7c546ff1
tshimakawa <tsukasa.soccer.0922@gmail.com>
2018-05-26 02:00:51 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const https = require('https');
+const fs = require('fs');
+
+const attend = require('./routes/attend');
+const logView = require('./routes/logView');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/attend',attend);
+app.use('/logView', logView);
+//app.login('/login',login);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+// サーバ立ち上げ
+app.listen(45000, ()=> {
+  console.log('Attendance Server');
+});
+
+module.exports = app;
---
+++ routes/attend.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'isdl-admin',
+  password : 'Isdl443@',
+  database : 'attendance_platform_db'
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+
+});
+
+router.post('/',function(req,res){
+	// const body=req.body.events[0];
+  console.log(req.headers);
+  const headers = req.headers;
+  const uid = headers.uid;
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth()+1;
+  const day = date.getDate();
+  connection.query(`INSERT INTO attendance_data(date,uid) VALUES("${year}-${month}-${day}","${uid}")`,function(error,result,fields){
+    if (error) throw error;
+    else{
+      console.log("success");
+    }
+  });
+
+	const response = {};
+	res.send(JSON.stringify(response));
+});
+
+module.exports = router;
