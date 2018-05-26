39dd155b6899aa7298dd4256f350bc5714e556db
tshimakawa <tsukasa.soccer.0922@gmail.com>
2018-05-26 13:09:16 +0900

    ミーティング日程取得機能完成

--- routes/meetingDate.js
+++ routes/meetingDate.js
@@ -19,6 +19,7 @@ router.get('/', function(req, res, next) {
       console.log(result);
 
       for(let i=0;i<result.length;i++){
+        result[i].date = formatDate(result[i].date, 'yyyy-MM-dd');//取得した講義日のフォーマットを変更
         dateList.push(result[i].date);
       }
 
@@ -29,5 +30,16 @@ router.get('/', function(req, res, next) {
     }
   });
 });
+
+function formatDate (date, format) {
+  format = format.replace(/yyyy/g, date.getFullYear());
+  format = format.replace(/MM/g, ('0' + (date.getMonth() + 1)).slice(-2));
+  format = format.replace(/dd/g, ('0' + date.getDate()).slice(-2));
+  format = format.replace(/HH/g, ('0' + date.getHours()).slice(-2));
+  format = format.replace(/mm/g, ('0' + date.getMinutes()).slice(-2));
+  format = format.replace(/ss/g, ('0' + date.getSeconds()).slice(-2));
+  format = format.replace(/SSS/g, ('00' + date.getMilliseconds()).slice(-3));
+  return format;
+};
 
 module.exports = router;
